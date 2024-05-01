89fefbb3f5799d2ee4fe26556a73ec8707ae9351
a-jaybird <jensena24@gmail.com>
2024-05-01 11:07:02 -0500

    made a number guesser

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int answer = 9;
+        
+        Console.WriteLine("Betcha can't guess my number:");
+        string guess = Console.ReadLine();
+        int number = int.Parse(guess);
+        
+        do{
+                
+            if (number < answer)
+            {
+                Console.WriteLine("too low; guess again:");
+                
+        
+                
+
+            }
+            else if(number > answer)
+            {
+                Console.WriteLine("too high; guess again:");
+
+            }
+
+            guess = Console.ReadLine();
+            number = int.Parse(guess);
+        
+        }while(number != answer);
+        
+        Console.WriteLine("Congrats, you got it!");
+           
+            
+            
+
+        
     }
 }

a74d9769b9d76b38f178f111acc5a2edab426a75
a-jaybird <jensena24@gmail.com>
2024-05-01 12:06:20 -0500

    learned about lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+      List<int> numbers;
+      numbers=new List<int>();
+       Console.WriteLine("put in a number");
+       int max = 0;
+        int total = 0;
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+        while ( number!= 0)
+        {   
+            Console.WriteLine("put in a number");
+            numbers.Add(number);
+            input = Console.ReadLine();
+            number = int.Parse(input);
+            
+            
+        }
+        foreach ( int num in numbers){
+            
+            total += num;
+            if (max < num){
+                max = num;
+            } 
+
+        }
+
+        {
+            Console.WriteLine($"the total of the list would be: {total}");
+            Console.WriteLine($"the average would be: {total/numbers.Count}");
+            Console.WriteLine($"The largest number in the list is: {max}");
+        }
     }
 }

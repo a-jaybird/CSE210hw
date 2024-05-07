6f600720fa6b5f48ae4c38e25845bcc3631accbe
a-jaybird <jensena24@gmail.com>
2024-05-07 09:41:20 -0500

    done for assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,36 @@ using System;
 
 class Program
 {
+    // Person person = new Person();
+    // person._givenName = "Joseph";
+    // person._familyName = "Smith";
+    // person.ShowWesternName();
+    // person.ShowEasternName();
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+         Resume resume1 =new Resume();
+        resume1._name ="Ned Flanders ";
+        // resume1._jobs = job1.Display()
+        resume1.Display();
+        Console.WriteLine("Jobs:");
+        Job job1 =new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company ="Rackspace";
+        job1._startYear = 2012;
+        job1._endYear=2014;
+        job1.Display();
+
+        Job job2 =new Job();
+        job2._jobTitle = "Nyan cat";
+        job2._company ="Boogle";
+        job2._startYear = 2014;
+        job2._endYear=2019;
+        job2.Display();
+
+        // Resume resume1 =new Resume();
+        // resume1._name ="Ned Flanders ";
+        // // resume1._jobs = job1.Display()
+        // resume1.Display();
+
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,11 @@
+public class Resume()
+{
+    public string _name;
+    public List<Job> _jobs;
+    public void Display()
+    {
+        Console.WriteLine($"name : {_name}");
+
+    }
+
+}
